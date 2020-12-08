34926c923ac4ac0f633631e9926f0d4a65bf94d1
Collin Porter <colls.potta@gmail.com>
2020-12-08 16:53:36 -0700

    created function to handle button click later, getting elements by ID, assigning values, created function to add new employee and validate that all fields required, push that data to the page

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,7 +1,10 @@
+// Import React, Component, MDBDataTable and other components
 import React, { Component } from 'react';
 import { MDBDataTable } from 'mdbreact';
 import Employees from './Employees';
+import Button from './Button';
 
+// Create class Component using state to hold employee data
 class Data extends Component {
   state = {
     columns: [
@@ -24,5 +27,40 @@ class Data extends Component {
     ],
     rows: Employees
   }
+
+  // A function that handles the button click to 'Add Employee'
+  // Gets employee data based on ID and assigns a value
+  buttonClick = e => {
+    this.addEmployee(
+      document.getElementById("name").value,
+      document.getElementById("position").value,
+      document.getElementById("office").value,
+      document.getElementById("age").value,
+    )
+    // Gets data based on ID and assigns the value to empty string
+      document.getElementById("name").value = "";
+      document.getElementById("position").value = "";
+      document.getElementById("office").value = "";
+      document.getElementById("age").value = "";
+  };
+
+  // Create addEmployee function and pass through data.Make sure that all fields are validated by sending an error message if not
+  addEmployee = (name, position, office, age) => {
+    if (name === "" || position === "" || office === "" || age === "") {
+      alert("Missing required fields!")
+      return;
+    }
+
+    Employees.push({
+      name: name,
+      position: position,
+      office: office,
+      age: age
+    })
+
+    this.setState({ rows: Employees });
+  };
+
+
 
 };
