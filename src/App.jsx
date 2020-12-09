ef4e4f9096e969608fff664b78561de1c6371cbd
Collin Porter <colls.potta@gmail.com>
2020-12-09 15:48:35 -0700

    formatted docs

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Body from './components/Body'
 function App() {
   return (
     <div className="App">
-        <Body/>
+      <Body />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,9 +4,9 @@ import Data from "./Data";
 
 // Create Card function and use props to get the specific employee data that we want to display on the page
 function Card(props) {
-    return <div className="card m-3 text-center" style={{width: "16rem"}} id={props.id}>
+    return <div className="card m-3 text-center" style={{ width: "16rem" }} id={props.id}>
         <h1 className="header">Employee Directory</h1>
-        <Data/>
+        <Data />
     </div>
 }
 
